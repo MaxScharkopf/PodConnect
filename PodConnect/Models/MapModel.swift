e391c0e351d165a88b50d557c5cf5bf9f701f760
Noah Hester <noahcoder11@gmail.com>
2026-03-13 22:54:19 -0700

    Add view model to retrieve map location info and data structures to hold the data

--- PodConnect/Models/MapModel.swift
+++ PodConnect/Models/MapModel.swift
@@ -7,24 +7,42 @@
 
 import Foundation
 
+// HTTP Response data structure
 typealias MapResponse = [MapCategory]
 
+/*
+ 
+ Location categories for reference
+ 
+ Bell Tower East
+ Recreation Areas
+ Bell Tower
+ Open Areas
+ Bell Tower West
+ Buildings and Spaces
+ Classrooms
+ Gardens
+ 
+ */
+
+// Data structure for JSON decoding
 struct MapCategory: Decodable {
-    let name: String?
-    let catId: Int?
+    let name: String
+    let catId: Int
     let children: Children?
 }
 
+// Child structure to match the JSON data
 struct Children: Decodable {
-    let locations: [Location]?
+    let locations: [MapLocation]?
     let categories: [MapCategory]?
 }
 
-struct Location: Decodable, Identifiable {
+// Location structure
+struct MapLocation: Decodable, Identifiable {
     let id: Int
     let name: String
     let lat: Double
     let lng: Double
     let catId: Int?
-    let mapId: Int?
 }
--- PodConnect/Screens/MapView.swift
+++ PodConnect/Screens/MapView.swift
@@ -8,7 +8,7 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    
+    @StateObject private var mapViewModel = MapViewModel()
     @StateObject private var locationManager = LocationManager()
     
     //csuci coordinates
@@ -22,28 +22,92 @@ struct MapView: View {
             longitudeDelta: 0.008
         )
     )
-
-       var body: some View {
-           Map(position: .constant(mapPosition))
-               .ignoresSafeArea()
-       }
     
-        private var mapPosition: MapCameraPosition {
-            
-            if let userCoord = locationManager.userLocation {
-                return .region(MKCoordinateRegion(
-                    center: userCoord,
-                    span: MKCoordinateSpan(
-                        latitudeDelta: 0.008,
-                        longitudeDelta: 0.008
-                    )
-                ))
-            } else {
-                return .region(defaultRegion)
-            }
-        }
-   }
+    @State private var position: MapCameraPosition
+    
+    init() {
+        position = MapCameraPosition.region(defaultRegion)
+    }
 
-   #Preview {
-       MapView()
-   }
+    var body: some View {
+        // Load until retrieval done or failure
+        if mapViewModel.isLoading {
+            Text("Loading campus locations...")
+        }else {
+            Map(position: $position) {
+                ForEach(mapViewModel.mapLocations) { location in
+                    if let category = mapViewModel.locationCategories[location.catId ?? 0] {
+                        
+                        let style = markerStyle(for: category)
+                        
+                        if category != "Classrooms" {
+                            Marker(
+                                location.name,
+                                systemImage: style.icon,
+                                coordinate: CLLocationCoordinate2D(
+                                    latitude: location.lat,
+                                    longitude: location.lng
+                                )
+                            )
+                            .tint(style.color)
+                        }
+                    }
+                }
+            }
+                .ignoresSafeArea()
+                // Show an error prompt if there is an error loading locations
+                .alert("Error",
+                       isPresented: Binding(
+                            get: { mapViewModel.errorMessage != nil },
+                            // Clear the error on dismissal
+                            set: { _ in mapViewModel.errorMessage = nil }
+                       )
+                ) {
+                    Button("OK", role: .cancel) {}
+                } message: {
+                    // Print the error message
+                    Text("\(mapViewModel.errorMessage ?? "") Cannot load map locations.")
+                }
+        }
+    }
+    
+    private var mapPosition: MapCameraPosition {
+        if let userCoord = locationManager.userLocation {
+            return .region(MKCoordinateRegion(
+                center: userCoord,
+                span: MKCoordinateSpan(
+                    latitudeDelta: 0.008,
+                    longitudeDelta: 0.008
+                )
+            ))
+        } else {
+            return .region(defaultRegion)
+        }
+    }
+    
+    func markerStyle(for category: String) -> (icon: String, color: Color) {
+        switch category {
+        case "Recreation Areas":
+            return ("american.football.fill", .green)
+            
+        case "Open Areas":
+            return ("person.2.fill", .green)
+            
+        case "Buildings and Spaces":
+            return ("building.2.fill", .blue)
+            
+        case "Gardens":
+            return ("leaf.fill", .green)
+            
+        case "Bell Tower", "Bell Tower East", "Bell Tower West":
+            return ("building.2.fill", .red)
+            
+        default:
+            return ("mappin", .gray)
+        }
+    }
+}
+
+#Preview {
+    MapView()
+}
